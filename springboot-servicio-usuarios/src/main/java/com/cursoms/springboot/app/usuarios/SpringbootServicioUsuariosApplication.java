8cfa223ce1011af87861aefc41ea2deab3bbe89a
luchogimenez <gimenezluis001@gmail.com>
2022-03-24 16:40:50 -0300

    Se implementa RestResource y UsuarioCommons

--- springboot-servicio-usuarios/src/main/java/com/cursoms/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ springboot-servicio-usuarios/src/main/java/com/cursoms/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.cursoms.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan({"com.cursoms.springboot.app.commons.usuarios.models.entity"})
 @SpringBootApplication
 public class SpringbootServicioUsuariosApplication {
 
--- springboot-servicio-usuarios/src/main/java/com/cursoms/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/cursoms/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -1,14 +1,17 @@
 package com.cursoms.springboot.app.usuarios.models.dao;
 
-import com.cursoms.springboot.app.usuarios.models.entity.Usuario;
+import com.cursoms.springboot.app.commons.usuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario,Long> {
 
-    public Usuario findByUsername(String username);
+    @RestResource(path = "buscan-username")
+    public Usuario findByUsername(@Param("username") String username);
 
     @Query("SELECT u FROM Usuario u WHERE u.username=?1")
     public Usuario obtenerPorUsername(String username);
