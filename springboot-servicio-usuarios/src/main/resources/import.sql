bbac3f5a5f2a58b242f8ce0aedf590e7031f6ed4
luchogimenez <gimenezluis001@gmail.com>
2022-03-25 22:14:25 -0300

    Se encripta password  en Usuarios

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','1234',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','1234',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$bPmqFOG.ic/m6LEGdVyRMeOz5hoy01jGl.Lpxim5GJEaTGaQetYfy',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$ZqgSt8jKkMMSfMiJg/1itOoeN2ML2w8juucLSKk9dKhbVsqukXqsi',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
